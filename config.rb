aa70939c96c485fed0e71d7659e8bed48d22e28a
gn0m30 <reggie@loco8.org>
2010-03-16 00:33:06 -0500

    this and that

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@
 # Require any additional compass plugins here.
 project_type = :stand_alone
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/jekyll"
 css_dir = "/public/stylesheets"
 sass_dir = "/jekyll/_sass"
 images_dir = "/public/images"
